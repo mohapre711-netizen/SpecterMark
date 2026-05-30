df846f27fdb364caefea3a16f5f7036c7885c676
mohapre711-netizen <mohapre711@gmail.com>
2026-05-30 23:30:58 +0100

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,24 @@
 package com.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import com.example.ExampleMod;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.client.render.VertexConsumerProvider;
 
-@Mixin(MinecraftServer.class)
+@Mixin(EntityRenderer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+
+    @Inject(at = @At("HEAD"), method = "render")
+    private void onRender(Object state, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
+        if (state instanceof PlayerEntityRenderState renderState) {
+            ExampleMod.LOGGER.info("Rendering player: " + renderState.name);
+        }
+    }
 }
